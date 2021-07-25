b12842192be03057a48e59636d35dd6ad13d229a
vinayakjadhav <vinayakrpjadhav@gmail.com>
2021-07-25 20:27:47 +0530

    grand finale submissions

--- src/Grand Finale/Problem 2/defendTheCountry.js
+++ src/Grand Finale/Problem 2/defendTheCountry.js
@@ -11,7 +11,7 @@ var noOfPhases;
 var noOfStates;
 
 // var myArgs = process.argv.slice(2);
-// // console.log('myArgs: ', myArgs);
+// // /// console.log('myArgs: ', myArgs);
 var timeTaken = 0;
 process.stdin.on('data', function(data) {
   input_stdin += data;
@@ -20,7 +20,7 @@ process.stdin.on('data', function(data) {
 
 process.stdin.on('end', function() {
   input_stdin_array = input_stdin.replace(/\r/g, '').split("\n");
-  //  // console.log(input_stdin_array);
+  //  // /// console.log(input_stdin_array);
 
   startTime = Date.now();
   //Write code here
@@ -29,9 +29,66 @@ process.stdin.on('end', function() {
   var sequence = [];
   input_currentline = 1;
 
-  sequence = input_stdin_array[input_currentline].split(" ");
+  sequence = input_stdin_array[input_currentline].split(" ").map(a => +a);
 
-  console.log("sequence ", sequence);
+  /// console.log("sequence ", sequence);
+  output = sequence[0];
+
+  var isContinuosNaturalNumbers = findifContinuousNaturalNumbers(sequence);
+  /// console.log("sequence ", sequence, isContinuosNaturalNumbers);
+
+  if (!isContinuosNaturalNumbers) {
+    var faultIndex = -1;
+    var fault = findFaultInContinuousNaturalNumbers(sequence, faultIndex);
+    /// console.log("fault ", fault, sequence);
+
+    //      prepend
+
+    var d = Math.abs(fault - 1);
+
+    var dt0 = d + "" + sequence[0];
+    var f0 = +dt0 + fault;
+    f0 = "" + f0;
+    var f1 = +dt0 + fault + 1;
+    f1 = "" + f1;
+    var t1 = "" + sequence[fault];
+    var t2 = "" + sequence[fault + 1];
+    /// console.log("f0, f1, sequence[fault], sequence[fault + 1]", f0, f1, t1, t2);
+    if (f0.split("").includes(t1) &&
+      f1.split("").includes(t2)) {
+      /// console.log("they are there");
+      output = d + "" + sequence[0];
+    } else {
+      output = sequence[0] + "" + d;
+    }
+
+  }
+
+  function findFaultInContinuousNaturalNumbers(sequence, faultIndex) {
+    if (sequence.length == 1) return faultIndex;
+    var s1 = sequence.slice(0, Math.floor(sequence.length / 2));
+    var s2 = sequence.slice(Math.ceil(sequence.length / 2), sequence.length);
+    /// console.log("s1", s1);
+    /// console.log("s2", s2)
+
+    if (!findifContinuousNaturalNumbers(s1)) {
+      faultIndex = faultIndex + 0;
+      return findFaultInContinuousNaturalNumbers(s1, faultIndex);
+    } else {
+      faultIndex = faultIndex + Math.ceil(sequence.length / 2);
+      return findFaultInContinuousNaturalNumbers(s2, faultIndex);
+    }
+  }
+
+  function findifContinuousNaturalNumbers(sequence) {
+    var nos = sequence.map(a => +a);
+    var sum = nos.reduce((acc, cur) => acc + cur, 0);
+    //  /// console.log("sum", sum);
+    var n = nos[nos.length - 1];
+    var k = nos[0];
+    var sumByFormula = ((n * (n + 1)) - ((k - 1) * k)) / 2;
+    return sum == sumByFormula;
+  }
 
   // for (var k = 0; k < noOfTestCases; k++) {
   //   findPrimeNumbers(k, testCases[k].L, testCases[k].R)
@@ -54,10 +111,10 @@ process.stdin.on('end', function() {
   // }
 
 
-  //// console.log(output);
+  //// /// console.log(output);
   process.stdout.write("" + output + "\n");
   timeTaken = Date.now() - startTime;
-  console.log(timeTaken / 1000 + "s");
+  /// console.log(timeTaken / 1000 + "s");
 
 
   function findPrimeNumbers(testCaseNo, start, end) {
--- src/Grand Finale/Problem 4/escape.js
+++ src/Grand Finale/Problem 4/escape.js
@@ -4,14 +4,14 @@ process.stdin.setEncoding('ascii');
 var input_stdin = "";
 var input_stdin_array = "";
 var input_currentline = 0;
-var output;
+var output = 1;
 
 var primeNumbers = [];
 var noOfPhases;
 var noOfStates;
 
 // var myArgs = process.argv.slice(2);
-// // console.log('myArgs: ', myArgs);
+// // /// console.log('myArgs: ', myArgs);
 var timeTaken = 0;
 process.stdin.on('data', function(data) {
   input_stdin += data;
@@ -22,49 +22,37 @@ process.stdin.on('data', function(data) {
 process.stdin.on('end', function() {
   input_stdin_array = input_stdin.replace(/\r/g, '').split("\n");
 
-  //  // console.log(input_stdin_array);
+  //  // /// console.log(input_stdin_array);
 
   //Write code here
   var noOfTestCases = input_stdin_array[0];
-  output = new Array(+noOfTestCases);
-  var testCases = [];
+  var P = noOfTestCases[0];
+  var L = noOfTestCases[1];
+  var Captain = [noOfTestCases[2], noOfTestCases[3]];
+  var posts = [];
+  var ropes = [];
   input_currentline = 1;
-  for (var i = 0; i < noOfTestCases; i++) {
-    var [l, r] = input_stdin_array[input_currentline].split(" ");
-    testCases.push({
-      L: +l,
-      R: +r
-    });
-    input_currentline++;
+  for (var i = 0; i < P; i++) {
+    posts[i] = input_stdin_array[input_currentline++].split(" ");
+  }
+  for (var i = P; i < L; i++) {
+    ropes[i] = input_stdin_array[input_currentline++].split(" ");
   }
 
-  // console.log("testCases ", testCases);
+  /// console.log("Captain ", Captain);
+  /// console.log("posts ", posts);
+  /// console.log("ropes ", ropes);
 
   for (var k = 0; k < noOfTestCases; k++) {
     findPrimeNumbers(k, testCases[k].L, testCases[k].R)
   }
 
-  for (var i = 0; i < primeNumbers.length; i++) {
-    var l = primeNumbers[i].length;
-    switch (l) {
-      case 0:
-        output[i] = -1;
-        break;
-      case 1:
-        output[i] = 0;
-        break;
-      default:
-        output[i] = primeNumbers[i][l - 1] - primeNumbers[i][0];
-    }
 
 
-  }
-
-
-  //// console.log(output);
-  process.stdout.write("" + output.join("\n") + "\n");
+  //// /// console.log(output);
+  process.stdout.write("" + output + "\n");
   // timeTaken = Date.now() - startTime;
-  // // console.log(timeTaken, " ms timeTaken");
+  // // /// console.log(timeTaken, " ms timeTaken");
 
 
   function findPrimeNumbers(testCaseNo, start, end) {
